9763e904b14c7fd8ac88d328e1806175c56b2187
Kevin Altman <kevin@thinkrelevance.com>
2012-03-29 17:18:17 -0400

    Adding Middleman-blog with dummy content

---
+++ site/config.rb
@@ -0,0 +1,22 @@
+activate :blog
+# set :blog_permalink, ":year/:month/:day/:title.html"
+# set :blog_summary_separator, /READMORE/
+# set :blog_summary_length, 500
+
+page "/feed.xml", :layout => false
+
+# custom middleman blog setup
+set :erb, :layout_engine => :slim
+set :blog_layout_engine, "slim"
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+  
+  # Minify Javascript on build
+  # activate :minify_javascript
+  
+  # Enable cache buster
+  # activate :cache_buster
+end
---
+++ site/source/2011/01/01/new-article.html.markdown
@@ -0,0 +1,11 @@
+--- 
+title: "New Article title"
+date: 28/03/2012
+---
+
+# Header of my article
+Content of my article
+
+* List item 1
+* List item 2
+* List item 3
